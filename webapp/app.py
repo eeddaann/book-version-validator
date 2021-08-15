639a67c49833d4466afc8d3f2cc82635d2efa7f0
eeddaann <edan100@gmail.com>
2021-08-16 01:02:44 +0300

    Add webapp

---
+++ webapp/app.py
@@ -0,0 +1,49 @@
+import imghdr
+import os, sys
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory,json,jsonify
+from werkzeug.utils import secure_filename
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # import from parent
+from iterate_pages import generate_change_lst, squeeze
+
+app = Flask(__name__)
+# app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.pdf',".PDF"]
+app.config['UPLOAD_PATH'] = 'uploads'
+txt = [['AIB blank', 19, (19, 19), '1']]
+'''
+['', 22, (22, 22), '1'],
+['0017 00-1/2 blank', 71, (71, 71), '1'],
+['0030 00-1 - 0030 00-2', 99, (99, 100), '1'],
+['0035 00-2', 116, (116, 116), '1'],
+['0035 00-5/6 blank', 119, (119, 119), '1'],
+['0035B 00-1/2 blank', 125, (125, 125), '1'],
+['0037 00-3 - 0037 00-4', 131, (131, 132), '1']]
+'''
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return "Invalid image", 400
+        upload_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        uploaded_file.save(upload_path)
+        data = squeeze(generate_change_lst(upload_path))
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return jsonify(json.dumps(data)), 200
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
---
+++ webapp/static/script.js
@@ -0,0 +1,120 @@
+pdfjsLib.GlobalWorkerOptions.workerSrc = "static/pdf.worker.js";
+
+	var myState = {
+		pdf: null,
+		currentPage: 1,
+		zoom: 1
+	}
+	function createTable(data) {
+	var table = document.getElementById('resultTable');
+	data.forEach(function(object) {
+		var tr = document.createElement('tr');
+		tr.innerHTML = '<td onclick="goToPage('+object[1]+')">' + object[0] + '</td>' +
+		'<td>' + object[3] + '</td>' + '<td>' + object[2] + '</td>'
+		table.appendChild(tr);
+	});
+
+	}
+
+	function goToPage(i) {
+	myState.currentPage = i;
+	document.getElementById("current_page").value = myState.currentPage;
+	render();
+	}
+
+	function render() {
+		myState.pdf.getPage(myState.currentPage).then((page) => {
+		
+			var canvas = document.getElementById("pdf_renderer");
+			var ctx = canvas.getContext('2d');
+	
+			var viewport = page.getViewport(myState.zoom);
+
+			canvas.width = viewport.width;
+			canvas.height = viewport.height;
+		
+			page.render({
+				canvasContext: ctx,
+				viewport: viewport
+			});
+		});
+	}
+
+	document.getElementById('go_previous').addEventListener('click', (e) => {
+		if(myState.pdf == null || myState.currentPage == 1) 
+			return;
+		myState.currentPage -= 1;
+		document.getElementById("current_page").value = myState.currentPage;
+		render();
+	});
+
+	document.getElementById('go_next').addEventListener('click', (e) => {
+		if(myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages) 
+			return;
+		myState.currentPage += 1;
+		document.getElementById("current_page").value = myState.currentPage;
+		render();
+	});
+
+	document.getElementById('current_page').addEventListener('keypress', (e) => {
+		if(myState.pdf == null) return;
+		
+		// Get key code
+		var code = (e.keyCode ? e.keyCode : e.which);
+		
+		// If key code matches that of the Enter key
+		if(code == 13) {
+			var desiredPage = 
+			document.getElementById('current_page').valueAsNumber;
+								
+			if(desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+				myState.currentPage = desiredPage;
+				document.getElementById("current_page").value = desiredPage;
+				render();
+			}
+		}
+	});
+
+	document.getElementById('zoom_in').addEventListener('click', (e) => {
+		if(myState.pdf == null) return;
+		myState.zoom += 0.5;
+		render();
+	});
+
+	document.getElementById('zoom_out').addEventListener('click', (e) => {
+		if(myState.pdf == null) return;
+		myState.zoom -= 0.5;
+		render();
+	});
+
+	Dropzone.autoDiscover = false;
+window.onload = function () {
+	var dropzoneOptions = {
+		timeout: -1,
+		init: function () {
+			this.on("success", function (file) {
+				console.log("success > " + file.name);
+				pdfjsLib.getDocument('./uploads/'+file.name).then((pdf) => {
+				myState.pdf = pdf;
+				render();
+				document.getElementById("BigTable").style.display = "block";
+				});
+			});
+			this.on("sending", function(file, xhr, formData){
+				xhr.onreadystatechange = function() {
+					if (xhr.readyState === 4) {
+						var response = JSON.parse(xhr.responseText);
+						if (xhr.status === 200) {
+							createTable(JSON.parse(response))
+							console.log('successful');
+						} else {
+							console.log('failed');
+						}
+					}
+				}
+			});
+		}
+	};
+	var uploader = document.querySelector('#uploader');
+	var newDropzone = new Dropzone(uploader, dropzoneOptions);
+}
